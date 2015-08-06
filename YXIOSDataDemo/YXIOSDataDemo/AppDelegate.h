c6c787bc7459a343125488ffc53b60f8c47d6c77
yixiang <yixiang.yx@alibaba-inc.com>
2015-08-06 19:23:13 +0800

    first commit

---
+++ YXIOSDataDemo/YXIOSDataDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXIOSDataDemo
+//
+//  Created by yixiang on 15/8/6.
+//  Copyright (c) 2015年 yixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXIOSDataDemo/YXIOSDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXIOSDataDemo
+//
+//  Created by yixiang on 15/8/6.
+//  Copyright (c) 2015年 yixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
